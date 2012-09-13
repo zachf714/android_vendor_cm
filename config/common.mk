9d5cece60446cd43aad876e6ce930da6f6b6d6bc
David Gasaway <ponsasinorem@gmail.com>
2012-09-13 03:36:42 -1000

    Add Goo Manager support
    Change-Id: Ifeca345479033df0641b2ec8803802e87f03b13f

--- config/common.mk
+++ config/common.mk
@@ -203,7 +203,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := PONS
     CM_EXTRAVERSION :=
 endif
 
@@ -216,3 +216,12 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(CM_VERSION) \
   ro.modversion=$(CM_VERSION)
+
+# Goo Manager support
+PRODUCT_PROPERTY_OVERRIDES += \
+ifeq ($(CM_BUILDTYPE),PONS)
+    PRODUCT_PROPERTY_OVERRIDES += \
+      ro.goo.developerid=PonsAsinorem \
+      ro.goo.rom=$(PRODUCT_VERSION_MAJOR)-$(CM_BUILDTYPE)- $(CM_BUILD)\
+      ro.goo.version=$(shell date +%s)
+endif
