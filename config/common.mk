c550dee16be022350e2e5cdd1d6ca60d923989ab
scotthartbti <scott.hart.bti@gmail.com>
2013-03-09 16:11:11 -0800

    Add xposedDPI to the build. Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -103,7 +103,9 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
     vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
     vendor/cm/prebuilt/hololauncherhd/HoloLauncherHD.apk:system/app/HoloLauncherHD.apk \
-    vendor/cm/prebuilt/ATweakerFree.apk:system/app/ATweakerFree.apk
+    vendor/cm/prebuilt/ATweakerFree.apk:system/app/ATweakerFree.apk \
+    vendor/cm/prebuilt/XposedDPI.apk:system/app/XposedDPI.apk \
+    vendor/cm/prebuilt/XposedInstaller.apk:system/app/XposedInstaller.apk
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
