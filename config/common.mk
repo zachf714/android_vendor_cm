99c303cf05782878e85788cbf30b0b39d46a53a5
scotthartbti <scott@ubuntu.com>
2013-06-30 14:51:26 -0400

    Update to BeanStalk 1.261 - RamBar bug fix and a couple picks. Signed-off-by: scotthartbti <scott@ubuntu.com>

--- config/common.mk
+++ config/common.mk
@@ -233,7 +233,7 @@ else
     CM_EXTRAVERSION :=
 endif
 
-Bean_Version=1.260
+Bean_Version=1.261
 CM_VERSION := BeanStalk$(Bean_Version)-$(shell date -u +%Y%m%d)-$(CM_BUILD)$(CM_EXTRAVERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
