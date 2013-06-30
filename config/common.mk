dc9e54ada2d418cc6de354e7801884cc3b8f3104
scotthartbti <scott@ubuntu.com>
2013-06-30 03:32:31 -0400

    BeanStalk 1.260
    Signed-off-by: scotthartbti <scott@ubuntu.com>

--- config/common.mk
+++ config/common.mk
@@ -233,8 +233,7 @@ else
     CM_EXTRAVERSION :=
 endif
 
-Bean_Version=1.253
-
+Bean_Version=1.260
 CM_VERSION := BeanStalk$(Bean_Version)-$(shell date -u +%Y%m%d)-$(CM_BUILD)$(CM_EXTRAVERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
