ed93372b4837ba34c7825ac197dd5179c1f986ec
scotthartbti <scott@ubuntu.com>
2013-07-10 01:27:10 -0400

    Bump to BeanStalk 1.275. Signed-off-by: scotthartbti <scott@ubuntu.com>

--- config/common.mk
+++ config/common.mk
@@ -219,7 +219,7 @@ else
     CM_EXTRAVERSION :=
 endif
 
-Bean_Version=1.261
+Bean_Version=1.275
 CM_VERSION := BeanStalk$(Bean_Version)-$(shell date -u +%Y%m%d)-$(CM_BUILD)$(CM_EXTRAVERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
