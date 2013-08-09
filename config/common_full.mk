48ff3e6e8b90f59188b7d69c7176dcdb9a6983a8
scott <scott@ubuntu.com>
2013-08-08 23:07:18 -0400

    remove videos.. need to free up some space. Signed-off-by: scott <scott@ubuntu.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -8,9 +8,6 @@ include frameworks/base/data/sounds/AudioPackage10.mk
 # Extra Ringtones
 include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
