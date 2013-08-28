b990371a8731847da636c43d702b87a7d8f1d24a
scott <scott@ubuntu.com>
2013-08-28 01:22:24 -0400

    Everyones favorite time... Bump!  BeanStalk 4.3.07 Signed-off-by: scott <scott@ubuntu.com>

--- config/common.mk
+++ config/common.mk
@@ -238,7 +238,7 @@ else
     CM_EXTRAVERSION :=
 endif
 
-Bean_Version=4.3.05
+Bean_Version=4.3.07
 CM_VERSION := BeanStalk-$(Bean_Version)-$(shell date -u +%Y%m%d)-$(CM_BUILD)$(CM_EXTRAVERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
