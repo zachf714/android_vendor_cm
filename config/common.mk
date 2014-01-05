53252f2d36426dd307d7e184883f714473eb664e
Steve Kondik <shade@chemlab.org>
2014-01-04 16:58:59 -0800

    cm: Remove legacy compcache stuff
     * Remove all this stuff.  If a device wants ZRAM, it should be
       enabled by the maintainer and properly configured and tuned.
     * This stuff currently causes a conflict with the ZRAM support
       added in Kitkat. Kill it.
    
    Change-Id: Ib2488ea4463e32ec44b65fe786f732145b5b6e23
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -142,11 +142,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.local.rc:root/init.cm.rc
 
-# Compcache/Zram support
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bin/compcache:system/bin/compcache \
-    vendor/cm/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
-
 # Copy JNI libarary of Term
 PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
