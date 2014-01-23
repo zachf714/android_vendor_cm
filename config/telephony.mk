fb13f00b6337a107e81e36adff8ce777cd4b81f0
Zach <zachf714@gmail.com>
2014-01-22 19:15:12 -0500

    No more whisperpush, just causes problems
    Change-Id: I141a2ebada9a2b3518eb0cb7c984b437ee738142

--- config/telephony.mk
+++ config/telephony.mk
@@ -15,8 +15,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Mms \
     Stk \
-    CellBroadcastReceiver \
-    WhisperPush
+    CellBroadcastReceiver
+#    WhisperPush
 
 # Mms depends on SoundRecorder for recorded audio messages
 PRODUCT_PACKAGES += \
