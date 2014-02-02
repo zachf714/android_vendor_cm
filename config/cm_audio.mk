c3ceef03f77d721a438c1b14e725a34c3fbb89cf
hany <hany.alsamman@gmail.com>
2014-02-02 13:28:30 +0000

    [2/2] Create SoundPack Folder and Install Default Pack
    Change-Id: If5b4519c5aa8c3e18d42b1a7ac411e7aae661dc1
    Signed-off-by: hany <hany.alsamman@gmail.com>

--- config/cm_audio.mk
+++ config/cm_audio.mk
@@ -35,3 +35,6 @@ PRODUCT_COPY_FILES += \
 	$(RINGTONE_PATH)/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg \
 	$(RINGTONE_PATH)/Silmarillia.ogg:system/media/audio/ringtones/Silmarillia.ogg
 
+# SoundPack
+PRODUCT_COPY_FILES += \
+	vendor/cm/prebuilt/common/media/audio/soundpack/plodeys1.zip:system/media/audio/plodeys.zip
