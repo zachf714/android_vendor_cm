d16d670b41bb9c9f7ff6d2eab3fb5897fa0e5da9
Zach <zachf714@gmail.com>
2014-02-16 18:45:12 -0500

    Remove the UI line because it may be overriding the actual UI audio
    Change-Id: If9407c5582dcb70fa2efb6f067d0b598171bbd4b

--- config/cm_audio.mk
+++ config/cm_audio.mk
@@ -5,7 +5,6 @@
 ALARM_PATH := vendor/cm/prebuilt/common/media/audio/alarms
 NOTIFICATION_PATH := vendor/cm/prebuilt/common/media/audio/notifications
 RINGTONE_PATH := vendor/cm/prebuilt/common/media/audio/ringtones
-UI_PATH := vendor/cm/prebuilt/common/media/audio/ui
 
 # Alarms
 PRODUCT_COPY_FILES += \
