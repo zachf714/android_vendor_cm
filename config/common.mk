c9ec283151561c3726e9a9345af73035b4242a83
scott <scott@ubuntu.com>
2014-03-18 01:38:22 -0400

    bump to beanstalk 4.4.270
    Change-Id: Ia539e8b5cc4482771e50e699f454a044ced5ceaf
    Signed-off-by: scott <scott@ubuntu.com>

--- config/common.mk
+++ config/common.mk
@@ -308,7 +308,7 @@ else
     CM_EXTRAVERSION :=
 endif
 
-Bean_Version=4.4.255
+Bean_Version=4.4.270
 CM_VERSION := BeanStalk-$(Bean_Version)-$(shell date -u +%Y%m%d)$(CM_EXTRAVERSION)-$(CM_BUILD)
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -317,7 +317,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
   ro.bs=true \
   ro.goo.developerid=beanstalk \
   ro.goo.rom=$(CM_BUILD) \
-  ro.goo.version=20
+  ro.goo.version=21
 
 -include vendor/cm-priv/keys/keys.mk
 
