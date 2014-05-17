0a434fface507344ce75ba311a6c689019f1d785
Zach <zachf714@gmail.com>
2014-05-16 23:41:20 -0400

    telephony.mk: Remove whisperpush
    Change-Id: I94571e2a93186b5a8704757542982a31819630ea

--- config/telephony.mk
+++ config/telephony.mk
@@ -16,8 +16,8 @@ PRODUCT_PACKAGES += \
     Mms \
     Stk \
     CellBroadcastReceiver \
-    VoiceDialer \
-    WhisperPush
+    VoiceDialer
+#    WhisperPush
 
 # Mms depends on SoundRecorder for recorded audio messages
 PRODUCT_PACKAGES += \
